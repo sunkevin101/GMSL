459f211f2f06dd11f5971227f79e5b42e9ff9afc
Kevin Sun <56786071+sunkevin101@users.noreply.github.com>
2025-12-10 10:55:49 +0800

    Update train_utils.py

--- utils/train_utils.py
+++ utils/train_utils.py
@@ -1,15 +1,3 @@
-# ====== Delayed Run ======
-from tqdm import tqdm
-import time
-
-def run_after_hours(delay_hours):
-    delay_minutes = int(delay_hours * 60)
-    print(f"\nrun the code after {delay_minutes} minutes")
-    if delay_minutes > 0:
-        for t in tqdm(range(delay_minutes)):
-            time.sleep(60)  # 1 min = 60 s
-
-
 # ====== Create New Directory ======
 import os
 def create_directory(save_path):
@@ -30,11 +18,4 @@ def print_result_as_table(result_dict, num_fold):
         print('+-------+-------------+--------------+')
 
 
-def print_result_as_table_finetune_model(result_dict, num_fold):
-    print('+-------+--------------+')
-    print('|  Fold | Best c-index |')
-    print('+-------+--------------+')
-    for i in range(num_fold):
-        print('|   {}   |    {:.4f}    |'.format(i + 1, result_dict[i][0]))
-        print('+-------+--------------+')
 
